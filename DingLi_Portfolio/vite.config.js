310f52d9a5c4fc775f7a8a0f46533a74ffe053b5
Ding Li <dl.dingli@outlook.com>
2024-10-17 08:09:32 -0400

    Vite.config.js change

--- DingLi_Portfolio/vite.config.js
+++ DingLi_Portfolio/vite.config.js
@@ -5,22 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
-  server: {
-    port: 3000
-  },
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, index.html)
     }
   }
 })
